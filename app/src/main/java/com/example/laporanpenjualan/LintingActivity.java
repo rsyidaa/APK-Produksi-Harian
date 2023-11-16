991cf2be91632f22238c32dbd38596d003c2d14f
Iskhand <marufiskandar13@gmail.com>
2023-11-16 11:43:32 +0700

    Mengubah file, menambahkan icon, menambah activity linting dan activity packing dan dashboard

---
+++ app/src/main/java/com/example/laporanpenjualan/LintingActivity.java
@@ -0,0 +1,14 @@
+package com.example.laporanpenjualan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LintingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linting);
+    }
+}
